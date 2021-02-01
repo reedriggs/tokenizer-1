c8f15a48e121bf4d74f1e80a12f9fe7b37287db7
Reed Riggs <78134431+reedriggs@users.noreply.github.com>
2021-01-31 15:17:58 -1000

    Add files via upload

--- checkNet.py
+++ checkNet.py
@@ -1,15 +1,50 @@
-#both of these now work on my iPhone
+#quickly check internet connection through three common web browser links
 
-def checkNet():
+def checkLink(link):
   import requests
+  link_a = "http://www." + str(link) + ".com"
   try:
-    response = requests.get("http://www.google.com")
-    print("response code: " + str(response.status_code))
+    response = requests.get(link_a)
+    #print("response code: " + str(response.status_code))
     return True
   except requests.ConnectionError:
-    print("Could not connect")
+    status_code = ("response code: " + str(response.status_code) + "; ")
+    print("Could not connect" + "\n" + "HTTP status codes: 200 OK, 301 Moved Permanently, 302 Found (Moved Temporarily), 401 Unauthorized, 403 Forbidden, 404 Not Found, 410 Gone, 500 Internal Server Error, 503 Service Unavailable")
+    return False
 
-checkNet()
+def CheckConnect():
+  #Bing showed the fastest response relative to Yahoo! and Duckduckgo, so checking Bing first should return the fastest response
+  if checkLink("bing") == True:
+    #print("Connected")
+    return True
+  elif checkLink("yahoo") == True:
+    #print("Connected")
+    return True
+  elif checkLink("duckduckgo") == True:
+    #print("Connected")
+    return True
+  else:
+    print("Problem Connecting to Internet")
+    return False
+
+def CheckCorpusSite(corpus_link):
+  if CheckConnect() == True:
+    import requests
+    link_b = str(corpus_link)
+    try:
+      response = requests.get(link_b)
+      #print("response code: " + str(response.status_code))
+      return True
+    except requests.ConnectionError:
+      print("Could not connect to corpus site")
+      return False
+  else:
+    print("Problem Connecting to Internet")
+    return False
+
+#first test to see if the operations above will work and in in their intended order
+BLCU_corpus_site = """http://202.112.195.8:8089/ccir_login?input=*"""
+CheckCorpusSite(BLCU_corpus_site)
 
 def internet_on():
   import requests
@@ -20,15 +55,15 @@ def internet_on():
       return True
     except urllib2.URLError as err: pass
   return False
-  print("Could not connect")
+  print("Could not connect" + "\n" + "HTTP status codes: 200 OK, 301 Moved Permanently, 302 Found (Moved Temporarily), 401 Unauthorized, 403 Forbidden, 404 Not Found, 410 Gone, 500 Internal Server Error, 503 Service Unavailable")
 
-internet_on()
+#internet_on()
 
-print("""HTTP status codes: 200 OK, 301 Moved Permanently, 302 Found (Moved Temporarily), 401 Unauthorized, 403 Forbidden, 404 Not Found, 410 Gone, 500 Internal Server Error, 503 Service Unavailable""")
+#print("\n" + "HTTP status codes: 200 OK, 301 Moved Permanently, 302 Found (Moved Temporarily), 401 Unauthorized, 403 Forbidden, 404 Not Found, 410 Gone, 500 Internal Server Error, 503 Service Unavailable")
 
-#next steps:
-#1. check if works on laptop computer
-#2. first check if internet is connected
+
+#first check if internet is connected
   #if so, check if corpus search returns results
     #if so, perform lemmatization and other analyses
-#3. gui offers Chinese corpus database clickboxes
+
+#gui offers Chinese corpus database clickboxes
